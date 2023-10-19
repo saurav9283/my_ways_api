4c25fb1d22cb27a3e01b768801e8ef6adf7cf0d4
Saurav <sauravkumar00108@gmail.com>
2023-10-19 23:26:34 +0530

    changes

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const userRoutes = require("../backend/route/userRoute.js");
-const chatRoute = require("../backend/route/chatRoute.js");
+const userRoutes = require("./route/userRoute.js");
+const chatRoute = require("./route/chatRoute.js");
 
 const app = express();
 require("dotenv").config();
@@ -12,25 +12,25 @@ app.use(cors());
 dotenv.config();
 app.use(express.json());
 
-// const connect = async () => {
-//   try {
-//     await mongoose.connect(process.env.MONGO, {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//     });
-//     console.log("Connected to MongoDB!");
-//   } catch (error) {
-//     console.error("Error connecting to MongoDB:", error);
-//   }
-// };
-// connect();
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connected to MongoDB!");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
+};
+connect();
 
 app.get("/", (req, res) => {
   res.send("API working");
 });
 
-// app.use("/api/auth", userRoutes);
-// app.use("/api/message", chatRoute);
+app.use("/api/auth", userRoutes);
+app.use("/api/message", chatRoute);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server started on port ${process.env.PORT}`);
